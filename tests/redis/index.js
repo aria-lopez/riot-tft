0ece8cd52151f6603e06614c9788f5afe53e894f
aria-lopez <aria.lopez.dev@gmail.com>
2021-07-02 20:51:46 -0700

    Redis class now accepts custom options, tests passing

--- tests/redis/index.js
+++ tests/redis/index.js
@@ -3,6 +3,18 @@ const { expect } = require('chai');
 const assert = require('assert');
 
 describe('Redis Cache', async () => {
+    const cache = new Redis();
+
+    it('Should be able to take dynamic options', () => {
+        const customOptions = {
+            host: '127.0.0.1',
+            port: 6379,
+            keyPrefix: 'custom-',
+        }
+        const customCache = new Redis(customOptions);
+        expect(customCache.key).to.equal(customOptions.keyPrefix);
+    });
+
     it('Should be able to store data with an expiry time', async () => {
         expect(0).to.equal(1);
     });
