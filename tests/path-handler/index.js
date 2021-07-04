ab3327408bf6e6ff3040e188ba3afacdda04cde3
aria-lopez <aria.lopez.dev@gmail.com>
2021-07-03 18:49:51 -0700

    Added leagueInfoBySummonerId to path class and tests are now passing

--- tests/path-handler/index.js
+++ tests/path-handler/index.js
@@ -75,7 +75,9 @@ describe('Path handler', () => {
     });
 
     it('League: Should create a valid leagueInfoBySummonerId path', () => {
-        expect(0).to.equal(1);
+        const expected = `https://na1.api.riotgames.com/tft/league/v1/entries/by-summoner/IFg_bFm3i52CJVxBvKBuqQmmZ_SqEq52nnInd-PAtfw?api_key=${API_KEY}`;
+        const actual = pathHandler.leagueInfoBySummonerId();
+        expect(actual).to.equal(expected);
     });
 
     it('League: Should create a valid leagueInfoByTierAndDivision path', () => {
