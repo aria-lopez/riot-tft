cf3668e6c64e4c8c85d1d83ed3ecd824b96d8b47
aria-lopez <aria.lopez.dev@gmail.com>
2021-07-05 16:57:49 -0700

    Added getSummonerBySummonerId to RiotTFT class and all tests passing

--- tests/source/index.js
+++ tests/source/index.js
@@ -10,7 +10,7 @@ describe('RiotTFT query tool', () => {
         summonerName: 'scarra',
         accountId: 'HZRCRfoL7vf8PmW9DFGeQnZx05AFW6dNgxP1t-N3Xw',
         puuid: 'vy9iRyl4SENb_sop5qRwuCmHqcV_YU1OKss9E8sbnLzNjA956jwHHfMvnBCx9DdCsSD6IK-gIrAj9g',
-
+        summonerId: 'IFg_bFm3i52CJVxBvKBuqQmmZ_SqEq52nnInd-PAtfw',
     };
     const useRedis = true;
     const redisConfig = { options: false, ttl: 10 };
@@ -31,9 +31,15 @@ describe('RiotTFT query tool', () => {
         assert.deepStrictEqual(actual, data);
     });
 
-    it('Summoner: Should be able to quyery summonerByPuuid', async () => {
+    it('Summoner: Should be able to query summonerByPuuid', async () => {
         const { data } = await axios.get(`https://na1.api.riotgames.com/tft/summoner/v1/summoners/by-puuid/vy9iRyl4SENb_sop5qRwuCmHqcV_YU1OKss9E8sbnLzNjA956jwHHfMvnBCx9DdCsSD6IK-gIrAj9g?api_key=${API_KEY}`);
         const actual = await tft.getSummonerByPuuid();
+        assert.deepStrictEqual(actual, data);
+    });
+
+    it('Summoner: Should be able to query summonerBySummonerId', async () => {
+        const { data } = await axios.get(`https://na1.api.riotgames.com/tft/summoner/v1/summoners/by-name/scarra?api_key=${API_KEY}`);
+        const actual = await tft.getSummonerBySummonerId();
         assert.deepStrictEqual(actual, data);
     });
 });
